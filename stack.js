628858bb23cce264d04e46957bdeee3d3da7f498
himani241 <72084226+himani241@users.noreply.github.com>
2020-09-29 21:27:25 +0530

    Update stack.js

--- stack.js
+++ stack.js
@@ -13,7 +13,7 @@ export default class Screen1 extends Component
 	{
 	    return(
 		<View>
-			<Text style={{textAlign:"center",fontsize:30}}>Screen1</Text>
+			<Text style={{textAlign:'center',fontsize:30}}>Screen1</Text>
 			<Button title = "Show" onPress={this.Show}></Button>
 		</View>
 	    );
@@ -38,7 +38,7 @@ export default class Screen2 extends Component
 	{
 	    return(
 		<View>
-			<Text style={{textAlign:"center",fontsize:30}}>Screen2</Text>
+			<Text style={{textAlign:'center',fontsize:30}}>Screen2</Text>
 			<Button title = "Show" onPress={this.Show}></Button>
 		</View>
 	    );
@@ -50,16 +50,16 @@ export default class Screen2 extends Component
 *App.js
 
 import React,{Component} from 'react';
-import { createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation ';
+import { createStackNavigator} from 'react-navigation-stack';
 
 import S1 from './Screen1';
 import S2 from './Screen2';
 
 const App=createStackNavigator(
 	{
-		S1:(screen:S1),
-		S2:(screen:s2),
+		S1:{screen:S1},
+		S2:{screen:S2},
 	}
 );
 export default createAppContainer(App);
